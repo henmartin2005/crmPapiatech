1a3f4d3bcd81ce6e31a91001dd829337718ce238
Henrry Martin <henmartin2005@gmail.com>
2026-02-11 22:51:16 -0500

    Fix: Explicitly set secret in authOptions for production

--- lib/auth.ts
+++ lib/auth.ts
@@ -13,6 +13,7 @@ export const authOptions: NextAuthOptions = {
     session: {
         strategy: "jwt",
     },
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         CredentialsProvider({
             name: "Sign in",
